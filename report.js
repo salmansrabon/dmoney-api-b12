a84f3e4ab9cb3100c974b2094878b91dd2ba01d1
salmansrabon <salmansrabon@gmail.com>
2024-08-30 22:39:02 +0600

    update collection file

--- report.js
+++ report.js
@@ -2,8 +2,8 @@ const newman = require('newman');
 require('dotenv').config();
  
 newman.run({
-    collection:`https://api.postman.com/collections/1844288-0c2f5c62-e7d8-4d31-8baa-6158bf88f4a0?access_key=${process.env.secretKey}`,
-    //collection:require('./collection/collection.json'),
+    //collection:`https://api.postman.com/collections/1844288-0c2f5c62-e7d8-4d31-8baa-6158bf88f4a0?access_key=${process.env.secretKey}`,
+    collection:require('./collection/collection.json'),
     reporters: 'htmlextra',
     iterationCount: 1,
     reporter: {
